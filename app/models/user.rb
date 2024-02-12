82a3a0c589653b60b39ea19285852b0619a49632
Ykgin <janvier20.yuyu.k@gmail.com>
2024-02-12 22:01:11 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :birthday, presence: true
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'は半角英数字で入力してください'
+
   has_many :schedules
   has_many :diaries
 end
