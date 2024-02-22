ec3e22b8ef977d1b716bd0d4920b5473b89dda75
Ykgin <janvier20.yuyu.k@gmail.com>
2024-02-22 22:13:50 +0900

    diaryモデルのテスト追加

--- spec/models/diary_spec.rb
+++ spec/models/diary_spec.rb
@@ -8,11 +8,19 @@ RSpec.describe Diary, type: :model do
   describe '日記登録' do
     context '日記を登録できる' do
       it 'すべての項目が存在すれば登録できる' do
-        
+        expect(@diary).to be_valid
       end
     end
     context '日記を登録できない' do
-      
+      it '日記が存在しなければ登録できない' do
+        
+      end
+      it 'ユーザーが紐付いていなければ登録できない' do
+        
+      end
+      it 'スケジュールが紐づいていなければ登録できない' do
+        
+      end
     end
   end
 end
